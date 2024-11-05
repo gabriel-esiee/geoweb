b16e17f3066b64eff0ecb4ee4d49f95477bd8c68
Gabriel <gabriel.rouleau@edu.esiee.fr>
2024-11-05 15:41:51 +0100

    Update database.

--- scripts/type_filter.js
+++ scripts/type_filter.js
@@ -1,42 +1,42 @@
 
-var selected_types = [];
+var selected_tags = [];
 
 const building_checkbox = document.getElementById("building-checkbox");
 const battle_checkbox = document.getElementById("battle-checkbox");
 const treaty_checkbox = document.getElementById("treaty-checkbox");
 const other_checkbox = document.getElementById("other-checkbox");
-const type_text_span = document.getElementById("type-text")
+const tag_text_span = document.getElementById("tag-text")
 
-building_checkbox.addEventListener('change', update_selected_types);
-battle_checkbox.addEventListener('change', update_selected_types);
-treaty_checkbox.addEventListener('change', update_selected_types);
-other_checkbox.addEventListener('change', update_selected_types);
+building_checkbox.addEventListener('change', update_selected_tags);
+battle_checkbox.addEventListener('change', update_selected_tags);
+treaty_checkbox.addEventListener('change', update_selected_tags);
+other_checkbox.addEventListener('change', update_selected_tags);
 
-function update_selected_types() {
-    selected_types = [];
+function update_selected_tags() {
+    selected_tags = [];
     count = 0;
 
     if(building_checkbox.checked) {
-        selected_types.push('building');
+        selected_tags.push('building');
         count++;
     }
 
     if(battle_checkbox.checked) {
-        selected_types.push('battle');
+        selected_tags.push('battle');
         count++;
     }
 
     if(treaty_checkbox.checked) {
-        selected_types.push('treaty');
+        selected_tags.push('treaty');
         count++;
     }
 
     if(other_checkbox.checked) {
-        selected_types.push('other');
+        selected_tags.push('other');
         count++;
     }
 
-    type_text_span.textContent = count;
+    tag_text_span.textContent = count;
 
     update_markers();
 }
